19a4efa99df5190986dee79463fb40b8b4402a4d
thehouseofcolors <rengin.celik97@gmail.com>
2025-09-21 18:20:19 +0300

    feature: movement added

--- RCShoter/Assets/Scripts/PlayerMovement.cs
+++ RCShoter/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public FloatingJoystick joystick;
+    public float speed;
+    Vector3 moveInput;
     void Update()
     {
-        
+        moveInput = new Vector3(joystick.Horizontal, joystick.Vertical, 0.00f);
+        transform.Translate(moveInput * speed * Time.deltaTime);
     }
 }
